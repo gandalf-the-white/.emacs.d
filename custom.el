bea2d75da645618c788b7124a23743fd6d8f61db
Laurent Valeyre <laurent.valeyre@orange.com>
2021-03-23 19:36:23 +0100

    step 2

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(doom-modeline ivy-rich counsel diminish doom-themes use-package)))
+   '(which-key rainbow-delimiters yasnippet-snippets yasnippet doom-modeline ivy-rich counsel diminish doom-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,9 +1,9 @@
 (setq inhibit-startup-message t)
 
-(setq url-proxy-services
-     '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
-         ("http" . "proxy.rd.francetelecom.fr:8080")
-         ("https" . "proxy.rd.francetelecom.fr:8080")))
+;; (setq url-proxy-services
+;;       '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
+;;         ("http" . "proxy.rd.francetelecom.fr:8080")
+;;         ("https" . "proxy.rd.francetelecom.fr:8080")))
 
 (menu-bar-mode -1)
 
@@ -16,8 +16,8 @@
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
+			             ("org" . "https://orgmode.org/elpa/")
+			             ("elpa" . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 
@@ -32,6 +32,20 @@
 (setq use-package-always-ensure t)
 
 ;;##################################
+;; LINE NUMBERS
+;;##################################
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda ()(display-line-numbers-mode 0))))
+
+;;##################################
 ;; GRAPHIC
 ;;##################################
 
@@ -42,6 +56,9 @@
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
 ;;##################################
 ;; IVY
@@ -56,17 +73,69 @@
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
-	:map ivy-minibuffer-map
-	("TAB" . ivy-alt-done)
-	("C-l" . ivy-alt-done)
-	("C-j" . ivy-next-line)
-	("C-k" . ivy-previous-line)
-	:map ivy-switch-buffer-map
-	("C-k" . ivy-previous-line)
-	("C-l" . ivy-done)
-	("C-d" . ivy-switch-buffer-kill)
-	:map ivy-reverse-i-search-map
-	("C-k" . ivy-previous-line)
-	("C-d" . ivy-reverse-i-search-kill))
+	     :map ivy-minibuffer-map
+	     ("TAB" . ivy-alt-done)
+	     ("C-l" . ivy-alt-done)
+	     ("C-j" . ivy-next-line)
+	     ("C-k" . ivy-previous-line)
+	     :map ivy-switch-buffer-map
+	     ("C-k" . ivy-previous-line)
+	     ("C-l" . ivy-done)
+	     ("C-d" . ivy-switch-buffer-kill)
+	     :map ivy-reverse-i-search-map
+	     ("C-k" . ivy-previous-line)
+	     ("C-d" . ivy-reverse-i-search-kill))
   :config
   (ivy-mode 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil))  ;; Don't start searches with ^
+
+;;##################################
+;; YASNIPPET
+;;##################################
+
+(use-package yasnippet
+  :ensure t
+  :config
+  (use-package yasnippet-snippets
+    :ensure t)
+  (yas-global-mode t)
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  (define-key yas-minor-mode-map (kbd "C-'") #'yas-expand)
+  (add-to-list #'yas-snippet-dirs "~/.emacs.d/snippets")
+  (yas-reload-all)
+  (setq yas-prompt-functions '(yas-ido-prompt))
+  (defun help/yas-after-exit-snippet-hook-fn ()
+    (prettify-symbols-mode)
+    (prettify-symbols-mode))
+  (add-hook 'yas-after-exit-snippet-hook #'help/yas-after-exit-snippet-hook-fn)
+  :diminish yas-minor-mode)
+
+;;##################################
+;; Indent
+;;##################################
+
+(setq-default tab-width 4
+	          indent-tabs-mode nil
+	          c-basic-offset 4)
+
+
+;;##################################
+;; Ctrl+h or Ctrl+x
+;;##################################
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config (setq which-key-idle-delay 0.3))
