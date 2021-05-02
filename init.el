2e86219530d009656eed3922c35de0c2015ed5af
Laurent Valeyre <laurent.valeyre@orange.com>
2021-05-02 17:35:27 +0200

    update

--- init.el
+++ init.el
@@ -14,3 +14,10 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/README.org")) 
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
