e609cdd8efdc78483add97e63841451571078c2f
spike spiegel <spikeandfaye@gmail.com>
2021-05-18 08:43:28 +0200

    update

--- init.el
+++ init.el
@@ -1,34 +1,33 @@
-(setq custom-file "~/.emacs-custom")
-(if (file-exists-p custom-file)
-    (load custom-file))
-
 (require 'package)
+
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
-
-(setq package-archives
-      '(("gnu"          . "https://elpa.gnu.org/packages/")
-        ("melpa-stable" . "https://stable.melpa.org/packages/")
-        ("org"          . "https://orgmode.org/elpa/")
-	    ("melpa"        . "https://melpa.org/packages/"))
-      package-archive-priorities
-      '(("melpa-stable" . 10)
-        ("gnu"          . 5)
-        ("melpa"        . 0)))
-
+ ;; (add-to-list 'package-archives
+ ;; 	     '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/README.org")) 
-
-(setq gc-cons-threshold (* 100 1024 1024)
-      read-process-output-max (* 1024 1024)
-      treemacs-space-between-root-nodes nil
-      company-idle-delay 0.0
-      company-minimum-prefix-length 1
-      lsp-idle-delay 0.1)  ;; clangd is fast
-
+(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-default-notes-file (concat org-directory "/notes.org"))
+ '(org-directory "~/Documents/orgfiles")
+ '(org-export-html-postamble nil)
+ '(org-hide-leading-stars t)
+ '(org-startup-folded 'overview)
+ '(org-startup-indented t)
+ '(package-selected-packages
+   '(dumb-jump projectile company lsp-ui lsp-mode cmake-font-lock cmake-mode magit beacon undo-tree yasnippet-snippets yasnippet flycheck zenburn-theme counsel ace-window org-ac org-bullets which-key try multiple-cursors use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
